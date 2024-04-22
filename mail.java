e91b02463d95fa432ff3f3a3b8f3f019cf7b7b33
MLHIPK <167212344+MLHIPK@users.noreply.github.com>
2024-04-22 23:07:08 +0300

    Create mail.java

---
+++ mail.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Erkek teamLead = new Erkek("Emre", "Keskin", "12/04/1994");
+        Departman departman = new Departman(teamLead);
+
+        Kadin personel1 = new Kadin("Meliha", "İpek", "20/04/1991");
+        departman.personelEkle(personel1);
+
+        Erkek personel2 = new Kadın("Elmas", "İpek", "17/07/1965");
+        departman.personelEkle(personel2);
+
+        departman.gorevEkle("Muhasebe Müdürü");
+        departman.gorevEkle("Developer");
+
+        System.out.println("Team Lead: " + departman.teamLead.ad + " " + departman.teamLead.soyad);
+        System.out.println("Personel Listesi: ");
+        for (Personel personel : departman.personelList) {
+            System.out.println(personel.ad + " " + personel.soyad);
+        }
+        System.out.println("Görevler: " + departman.assignmentList);
+    }
+}
